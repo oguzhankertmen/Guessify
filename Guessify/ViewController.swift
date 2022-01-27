0b7ed1b46c4f800dad875a5380a66dd0a5bdef37
Oğuzhan KERTMEN <iletisim@oguzhankertmen.com.tr>
2022-01-27 14:52:34 +0300

    Initial Commit

---
+++ Guessify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Guessify
+//
+//  Created by Oğuzhan KERTMEN on 25.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
